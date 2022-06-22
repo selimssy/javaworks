cdff1332d62d26f7226dbc469289673f92d92a4a
selimssy <selimssy@naver.com>
2022-06-22 09:58:40 +0900

    hello

---
+++ Hello/src/Hellojava.java
@@ -0,0 +1,13 @@
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//출력문
+		System.out.println("Hello~ Java");
+		
+		System.out.println("안녕~ 자바");
+	}
+
+}
