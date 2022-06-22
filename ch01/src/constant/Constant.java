4de59b44a492b088015b36737f0b5db5e130ca49
selimssy <selimssy@naver.com>
2022-06-22 10:44:20 +0900

    constant update

---
+++ ch01/src/constant/Constant.java
@@ -0,0 +1,34 @@
+package constant;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// 상수 - 변하지 않는 값
+		// 상수 선언할 때는 앞에 final 키워드 사용!
+		
+		final int MONTH = 12;
+		final int MAX_NUM = 100;
+		final int MIN_NUM = 1;
+		
+		//month = 6;   // final 사용하면(상수) 변경할 수 없다!
+		//MAX_NUM = 1000;   // 상수라서 변경 불가
+		
+		
+		System.out.println(MONTH);    // 12
+		System.out.println(MAX_NUM);    // 100
+		
+		
+		// 원의 넓이 계산하기
+		// area = radius * radius * PI
+		
+		final double PI= 3.14;    // 상수
+		int radius = 5;
+		double area = 0.0;
+		
+		// 연산 처리
+		area = radius * radius * PI;
+		
+		System.out.println("원의 넓이: " + area);     // 78.5
+	}
+
+}
